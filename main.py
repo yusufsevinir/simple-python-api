5f7332cc50a01bb9e95d9ae3cf6488cd7811e102
Yusuf Sevinir <yusufsevinir@Yusufs-MacBook-Pro.local>
2024-06-16 18:10:16 +0300

    test cases are added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HttpException
+from fastapi import FastAPI, HTTPException, status
 import requests
 
 app = FastAPI()
@@ -11,22 +11,20 @@ def read_status():
 @app.get("/posts")
 def get_posts():
     response = requests.get(url)
-    return response.json
+    return response.json()
 
 @app.get("/posts/{post_id}")
 def get_post(post_id: int):
     response = requests.get(url, post_id)
     if response.status_code == 200:
-        return response.json
+        return response.json()
     else:
-        raise HttpException(status_code=404, detail="Post not found")
+        raise HTTPException(status_code=404, detail="Post not found")
 
-@app.post("posts/")    
-def create_post(posts: dict):
-    response = requests.post(url, posts)
+@app.post("/posts", status_code= status.HTTP_201_CREATED)
+def create_post(post: dict):
+    response = requests.post(url, json=post)
     if response.status_code == 201:
-        return response.json
+        return response.json()
     else:
-        raise HttpException(status_code=404, detail="can not create post")
-    
-
+        raise HTTPException(status_code=response.status_code, detail="Failed to create post")
--- tests/test_main.py
+++ tests/test_main.py
@@ -7,5 +7,63 @@ client = TestClient(app)
 
 def test_read_status():
     response = client.get("/status")
-    assert response.status_code, 200
-    assert response.json, {"status":"ok"}
+    assert response.status_code == 200
+    assert response.json() == {"status":"ok"}
+    
+def test_get_posts():
+    with patch('requests.get') as mocked_get_posts:
+        mocked_get_posts.return_value.status_code = 200
+        mocked_get_posts.return_value.json.return_value = [
+            {"id": 1, "title": "Post 1", "body": "Content 1", "userId": 1},
+            {"id": 2, "title": "Post 2", "body": "Content 2", "userId": 2}
+        ]
+        response = client.get("/posts")
+        posts = response.json()
+        
+        print(f"Response JSON: {posts}")
+        assert response.status_code == 200
+        assert len(posts) == 2
+        assert isinstance(posts, list)
+
+def test_get_post():
+    with patch('requests.get') as mocked_get_post:
+        mocked_get_post.return_value.status_code = 200
+        mocked_get_post.return_value.json.return_value = [
+            {"id": 1, "title": "Post 1", "body": "Content 1", "userId": 1},
+        ]
+        response = client.get("/posts/1")
+        post = response.json()
+        
+        print(f"Response JSON: {post}")
+        assert response.status_code == 200
+        assert len(post) == 1
+        assert isinstance(post, object)
+        
+        mocked_get_post.return_value.status_code = 404
+        response = client.get("/posts/99")
+        assert response.status_code == 404
+        assert response.json() == {"detail":"Post not found"}
+        
+def test_create_post():
+    with patch('requests.post') as mocked_post:
+        post = {"title": "Post 355", "body": "Content 355", "userId": 355}
+        mocked_post.return_value.status_code = 201
+        mocked_post.return_value.json.return_value = {
+            "id": 101,
+            "title": "Post 355",
+            "body": "Content 355",
+            "userId": 355
+        }
+        
+        response = client.post("/posts", json=post)
+        created_post = response.json()
+        print(f"Response JSON: {created_post}")
+
+        assert response.status_code == 201
+        assert created_post["id"] == 101
+        
+        mocked_post.return_value.status_code = 404
+        response = client.post("/posts", json=post)
+        
+        assert response.status_code == 404
+        assert response.json() == {"detail":"Failed to create post"}
